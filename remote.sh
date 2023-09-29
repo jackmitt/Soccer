e332b39b950140ad3bd351f8c835e22170231d50
jackmitt <jjmittenthal@crimson.ua.edu>
2023-09-29 13:32:27 -0500

    clean up

--- remote.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sudo systemctl enable --now xrdp
-sudo ufw allow from any to any port 3389 proto tcp
--- v1.0/fixDates.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-df = pd.read_csv('./LaLiga_Csvs/understatGameStats.csv', encoding = "ISO-8859-1")
-for index, row in df.iterrows():
-    df.at[index, "Date"] = row["Date"].split()[0] + " " + row["Date"].split()[1] + ", " + row["Date"].split()[2]
-df.to_csv('./LaLiga_Csvs/understatGameStats.csv')
